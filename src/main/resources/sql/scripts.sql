0338dc8992f9d80aa213c7d3f06e0ead3f5950c1
Rafael Jesus <rafael_jesus86@hotmail.com>
2012-09-12 00:59:02 -0300

    deploy

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -1,7 +1,7 @@
 
 -- user  
-insert into scrum.user values (1, 'São José dos Campos', 'Rua Francisca Maria de Jesus', 'SP', 'adm', 'Rafael Jesus', 'password', 'MASTER');
-insert into scrum.user values (2, 'São José dos Campos', null, 'SP', 'adm', 'Barbara Veloso', 'password', 'MASTER');
+insert into scrum.user values (1, 'adm', 'Rafael Jesus', 'password', 'MASTER');
+insert into scrum.user values (2, 'adm', 'Barbara Veloso', 'password', 'MASTER');
 
 -- project
 insert into scrum.project values (1, 'Vitae Featurekids', 'a management system for hospitals', '2012-08-05', 'Salute');
@@ -12,11 +12,11 @@ insert into scrum.project values (5, 'OCPSoft', 'a management system for managin
 
 
 -- sprint
-insert into scrum.sprint values (1, '09:00', '2012-12-01', 'login implementation', 'Iteration 01', new Date(), 1);
-insert into scrum.sprint values (2, '09:00', '2012-12-01', 'selenium tests implementation', 'Iteration 02', new Date(), 1);
-insert into scrum.sprint values (3, '09:00', '2012-12-01', 'user view implementation', 'Iteration 03', new Date(), 1);
-insert into scrum.sprint values (4, '09:00', '2012-12-01', 'user crud implementation', 'Iteration 04', new Date(), 1);
-insert into scrum.sprint values (5, '09:00', '2012-12-01', 'arquillian crud tests implementation', 'Iteration 05', new Date(), 1);
+insert into scrum.sprint values (1, '09:00', '2012-12-01', 'login implementation', 'Iteration 01', '2012-12-15' , 1);
+insert into scrum.sprint values (2, '09:00', '2012-12-05', 'selenium tests implementation', 'Iteration 02', '2012-12-20', 1);
+insert into scrum.sprint values (3, '09:00', '2013-01-10', 'user view implementation', 'Iteration 03', '2013-01-25', 1);
+insert into scrum.sprint values (4, '09:00', '2013-01-15', 'user crud implementation', 'Iteration 04', '2013-02-01', 1);
+insert into scrum.sprint values (5, '09:00', '2013-02-01', 'arquillian crud tests implementation', 'Iteration 05', '2013-02-27', 1);
 
 -- task
 insert into scrum.sprint values (1, '00:45', 1, 1, 'any user story', 1);
